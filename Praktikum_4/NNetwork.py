9b598b001671f4df2e920798d2e258a0a7839452
Dumitru <63454161+LopushanD@users.noreply.github.com>
2024-07-06 11:03:30 +0200

    Praktikum 4 is finished

--- Praktikum_4/NNetwork.py
+++ Praktikum_4/NNetwork.py
@@ -1,28 +1,22 @@
 import time
 import numpy as np
-import math
 import pandas as pd
 import matplotlib.pyplot as plt
+import pickle
 
 class NeuralNetwork():
 
     #initialize the neural network
     def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
-        #random(rows,cols), return ndarray
         self.inputToHiddenWeights = np.random.random((inputnodes,hiddennodes))
         self.hiddenToOutputWeights = np.random.random((hiddennodes,outputnodes))
         self.learningRate = learningrate
-        
 
-    # def sigmoid(self, x:np.ndarray):
-    #     # result = 1 / (1+pow(1/math.e,x))
-    #     result = 1 / (1+np.exp(-x))
-    #     # print(f"AFTER CONVERTION {result}")
-    #     return result
     
     # #sigmoid function
     def sigmoid(self, x: np.ndarray):
         # Clip the input values to a range that avoids overflow in the exp function (works also without it, but warning appears)
+        # for exponent is same as infinity or zero
         x = np.clip(x, -709, 709)
         result = 1 / (1 + np.exp(-x))
         return result
@@ -36,19 +30,20 @@ class NeuralNetwork():
     
     #train the neural network
     def train(self, inputs:np.ndarray, targets:np.ndarray,learningRate):
-        # vectorized_sig_deriv = np.vectorize(self.sigmoid_derivative)
-        for iterations in range(10**3): 
-                                    #learning rate, not iterations
+        for iterations in range(16000): 
+            
             hiddenInputs = self.think(inputs,self.inputToHiddenWeights)
             # print(np.shape(hiddenInputs))
             predicted = self.think(hiddenInputs,self.hiddenToOutputWeights) # array
             # print(np.shape(predicted))
             absErrorVect = np.subtract(targets,predicted)
             hiddenErrorVect = np.dot(absErrorVect,self.hiddenToOutputWeights.T)
-            deltaWHiddenOutpMatrix = np.dot(hiddenInputs.T,np.multiply(absErrorVect,self.sigmoid_derivative(predicted))) # check if calculations here correct
-            self.hiddenToOutputWeights+= np.multiply(learningRate,deltaWHiddenOutpMatrix)#update weights after hidden layer
+            deltaWHiddenOutpMatrix = np.dot(hiddenInputs.T,np.multiply(absErrorVect,self.sigmoid_derivative(predicted))) 
+            #update weights after hidden layer
+            self.hiddenToOutputWeights+= np.multiply(learningRate,deltaWHiddenOutpMatrix)
             deltaWInputHiddenMartix = np.dot(inputs.T,np.multiply(hiddenErrorVect,self.sigmoid_derivative(hiddenInputs)))
-            self.inputToHiddenWeights+= np.multiply(learningRate,deltaWInputHiddenMartix) # update weights after input layer
+            #update weights after hidden layer
+            self.inputToHiddenWeights+= np.multiply(learningRate,deltaWInputHiddenMartix)
             
             
     #one calculation step of the network
@@ -59,13 +54,12 @@ class NeuralNetwork():
             np.ndarray: array of in-between/output assumptions [0;1]
         """
         multiplied = np.dot(inputs,weights) #array x array  = array
-        # vectorizedSigmoid = np.vectorize(self.sigmoid) # Makes scalar function to a vectorized one . upd not needed, vectorized by default
         result = self.sigmoid(multiplied) 
         return result
     
     def recognize(self,inputs:np.ndarray)-> np.ndarray:
         hiddenInputs = self.think(inputs,self.inputToHiddenWeights)
-        predicted = self.think(hiddenInputs,self.hiddenToOutputWeights) # array
+        predicted = self.think(hiddenInputs,self.hiddenToOutputWeights)
         
         return predicted
   
@@ -78,6 +72,17 @@ def oneHotEncode(labels, num_classes):
     for i, label in enumerate(labels):
         one_hot[i][label] = 0.99
     return one_hot
+
+def writeNetworkToFile(network:NeuralNetwork,filename:str="D:/downloads/P4_Dataset/TrainedNeuralNetwork.pkl")->None:
+        with open(filename, 'wb') as file:
+            pickle.dump(network, file)
+            file.close()
+            
+def loadNetworkFromFile(filename:str="D:/downloads/P4_Dataset/TrainedNeuralNetwork.pkl")->NeuralNetwork:
+    with open(filename, 'rb') as file:
+        loadedNeuralNetwork = pickle.load(file)
+        file.close()
+    return loadedNeuralNetwork
         
 if __name__ == "__main__":
 
@@ -85,30 +90,21 @@ if __name__ == "__main__":
     hidden_nodes = 200 #voodoo magic number
     output_nodes = 10 #numbers from [0:9]
 
-    learning_rate = 0.1 #feel free to play around with
+    learning_rate = 0.03 #feel free to play around with
 
-    # training_data_file = open("D:/downloads/P4_Dataset/mnist_train_100.csv")
-    # data is looks like ['1,230,0,33,0\n', '2,93,0,0,232\n', '...']
     # first number is label, other ones are pixel values
-    # training_data_list = training_data_file.readlines()
-    # training_data_file.close()
-    
-    # data is looks like ['1,230,0,33,0\n', '2,93,0,0,232\n', '...']
-    # first number is label, other ones are pixel values
-    TrainingData = pd.read_csv("D:/downloads/P4_Dataset/mnist_train_100.csv",header=None)
+    TrainingData = pd.read_csv("D:/downloads/P4_Dataset/mnist_train_full.csv",header=None)
     TrainingTargets = TrainingData.iloc[:,0].values
     TrainingInputs = TrainingData.iloc[:,1:].values
     TrainingInputs = normalizeInputs(TrainingInputs)
     TrainingTargets = oneHotEncode(TrainingTargets,10)
     
-    
-    TestData = pd.read_csv("D:/downloads/P4_Dataset/mnist_test_10.csv",header=None)
+    TestData = pd.read_csv("D:/downloads/P4_Dataset/mnist_test_full.csv",header=None)
     TestTargetsRaw = TestData.iloc[:,0].values
     TestInputsRaw = TestData.iloc[:,1:].values
     TestInputs = normalizeInputs(TestInputsRaw)
     TestTargets = oneHotEncode(TestTargetsRaw,10)
-    # print(TestTargets.tolist()[0])
-    # print(f"TEST INPUTS {np.shape(TestInputs)}\nTEST TARGETS {np.shape(TestTargets)}\n TRAINING INPUTS {np.shape(TrainingInputs)}\nTRAINING TARGETS {np.shape(TrainingTargets)}\n")  
+   
     n = NeuralNetwork(input_nodes,hidden_nodes,output_nodes, learning_rate)
     start = time.time()
     n.train(TrainingInputs,TrainingTargets,learning_rate)
@@ -117,16 +113,17 @@ if __name__ == "__main__":
     
     
     results = n.recognize(TestInputs)
-    # print(TestTargets.tolist())
     totalRecognitions = len(TestInputs)
-    correctRecognitions = 0
-    # print(results.__len__())
-    whatPredicted = results.max(0)
-    whatCorrect = TestTargets.max(0) # returns max value from each example in ndarray
-    #Now need to find index of this value (index = number that is correct)
-    #do same for predicted number and compare those indexes. if same -> predicted correctly, otherwise incorrectly
-        
-    image_array = np.reshape(TestInputsRaw[1],(28,28)) # print 2nd example
-    plt.imshow(image_array,cmap='Greys', interpolation='None')
-    plt.show()
+    
+    whatPredicted = np.argmax(results,axis=1) # returns index of max value from each example in ndarray
+    whatCorrect = np.argmax(TestTargets,axis=1) # returns index of max value from each example in ndarray
+    # print(f"{whatPredicted}"+'\n'+f"{whatCorrect}")
+    correctRecognitions = np.sum((whatPredicted == whatCorrect)) #compare indexes and sum up matches
+    accuracyPercentage = correctRecognitions/totalRecognitions*100
+    print(f" The accuracy of prediction is {accuracyPercentage}%")
+    writeNetworkToFile(n)
+    
+    # image_array = np.reshape(TestInputsRaw[1],(28,28)) # print 2nd example
+    # plt.imshow(image_array,cmap='Greys', interpolation='None')
+    # plt.show()
     
--- test.py
+++ test.py
@@ -1,15 +1,64 @@
-def toHorn(clause:str)-> list[str]:
-        clausePartitionedList = clause.split(" V ")
-        resultList = []
-        for i in range(0,len(clausePartitionedList)):
-            tmp= clausePartitionedList[i]
-            for j in range(0,len(clausePartitionedList)):
-                if(i==j):
-                    continue
-                tmp = tmp+' /\\ '+'-'+clausePartitionedList[j]
-            resultList.append(tmp)
-        return resultList
-    
-test = "P12 V P22 V P33"
+from Praktikum_4.NNetwork import *
+import os
+from PIL import Image
 
-print(toHorn(test)[0])
+TestData = pd.read_csv("D:/downloads/P4_Dataset/MyOwnImages.csv",header=None)
+# TestData = pd.read_csv("D:/downloads/P4_Dataset/mnist_test_full.csv",header=None)
+TestTargetsRaw = TestData.iloc[:,0].values
+TestInputsRaw = TestData.iloc[:,1:].values
+print(TestData)
+TestInputs = normalizeInputs(TestInputsRaw)
+TestTargets = oneHotEncode(TestTargetsRaw,10)
+
+network = loadNetworkFromFile()
+
+results = network.recognize(TestInputs)
+totalRecognitions = len(TestInputs)
+
+whatPredicted = np.argmax(results,axis=1) # returns index of max value from each example in ndarray
+whatCorrect = np.argmax(TestTargets,axis=1) # returns index of max value from each example in ndarray
+print(f"{whatPredicted}"+'\n'+f"{whatCorrect}")
+correctRecognitions = np.sum((whatPredicted == whatCorrect)) #compare indexes and sum up matches
+accuracyPercentage = correctRecognitions/totalRecognitions*100
+print(f" The accuracy of prediction is {accuracyPercentage}%")
+
+# print(TestInputs)
+# print(whatPredicted)
+
+# CODE TO CONVERT OWN IMAGES TO CSV FILE
+
+def image_to_flat_array(image_path,size=(28,28)):
+    """Converts an image to a flattened array of pixel values."""
+    image = Image.open(image_path).convert('L')  # Convert to grayscale
+    image = image.resize(size,Image.LANCZOS)
+    image_array = np.array(image)
+    flat_array = image_array.flatten()
+    return flat_array
+
+def images_to_csv(image_folder, csv_filename):
+    """Converts all images in a folder to a CSV file with flattened pixel arrays."""
+    # List to hold all flattened image arrays
+    image_data = []
+
+    # Iterate over all files in the image folder
+    for filename in os.listdir(image_folder):
+        
+        if filename.endswith('.png'):
+            image_path = os.path.join(image_folder, filename)
+            flat_array = image_to_flat_array(image_path)
+            np.insert(flat_array,0,filename[0])
+            image_data.append(flat_array)
+
+    # Convert list to DataFrame
+    image_df = pd.DataFrame(image_data)
+    
+
+    # Save DataFrame to CSV
+    return image_df.to_csv(csv_filename, index=False, header=False)
+
+# # # Example usage
+# image_folder = "D:/downloads/P4_Dataset"  # Replace with the path to your folder containing PNG images
+# csv_filename = 'D:/downloads/P4_Dataset/MyOwnImages.csv'  # Output CSV file name
+# print(images_to_csv(image_folder, csv_filename))
+
+
